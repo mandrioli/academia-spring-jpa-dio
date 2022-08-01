c91594a583a25772de7da4efdb8e3044ef39b3a6
Marcel Andrioli <marcelandrioli@gmail.com>
2022-08-01 15:48:03 -0300

    Avaliacoes

--- src/main/java/me/dio/academia/digital/repository/AvaliacaoFisicaRepository.java
+++ src/main/java/me/dio/academia/digital/repository/AvaliacaoFisicaRepository.java
@@ -1,4 +1,9 @@
 package me.dio.academia.digital.repository;
 
-public interface AvaliacaoFisicaRepository {
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // annotation opcional
+public interface AvaliacaoFisicaRepository extends JpaRepository<AvaliacaoFisica, Long> {
 }
